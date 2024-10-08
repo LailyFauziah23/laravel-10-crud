d3bb6f757e9fe9d7b1cecd3ed433f9fb163ed8d2
LailyFauziah23 <lailynuriyatulfauziah@mail.ugm.ac.id>
2024-10-08 14:59:03 +0700

    uts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', Controller::class);
+Route::resource('/products', ProductController::class);
